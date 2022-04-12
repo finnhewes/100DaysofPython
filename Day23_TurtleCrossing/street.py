a2d944d5e3c1856fe99dfa34aababc7da31fdf32
finnhewes <finnhewes@gmail.com>
2022-04-12 12:03:30 +0300

    Create street.py

---
+++ Day23_TurtleCrossing/street.py
@@ -0,0 +1,44 @@
+# our street graphic/game board. Just some extra styling, none of these are functional objects.
+from turtle import Turtle
+
+# these refer to the center y-pos of our lanes
+Y_LIST = [220, 110, 0, -110, -220]
+
+# this is the "pavement"
+class Street(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.penup()
+        self.home()
+        self.shape("square")
+        self.color("black")
+        self.shapesize(stretch_len=100, stretch_wid=29)
+
+# these are the far-side boundaries of the road
+class SolidLine(Turtle):
+    def __init__(self, y_cor):
+        super().__init__()
+        self.penup()
+        self.shape("square")
+        self.color("gold")
+        self.shapesize(stretch_wid=.175, stretch_len=100)
+        self.goto(0, y_cor)
+
+# these are the dashed lane markers
+class DashedLine(Turtle):
+    def __init__(self, y_cor):
+        super().__init__()
+        self.shape("square")
+        self.color("white")
+        self.shapesize(stretch_wid=.175, stretch_len=2.5)
+        self.goto(0, y_cor)
+
+# this is the pavement within the lanes.
+class Lane(Turtle):
+    def __init__(self, y_cor):
+        super().__init__()
+        self.penup()
+        self.shape("square")
+        self.color("dimgray")
+        self.shapesize(stretch_wid=5, stretch_len=100)
+        self.goto(0, y_cor)
