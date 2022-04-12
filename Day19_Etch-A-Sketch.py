f022b26dd518c822df51d4b7ea480a61f3b861cd
finnhewes <finnhewes@gmail.com>
2022-04-12 12:11:31 +0300

    Update and rename etch-a-sketch.py to Day19_Etch-A-Sketch.py

--- Day19_Etch-A-Sketch.py
+++ Day19_Etch-A-Sketch.py
@@ -1,9 +1,13 @@
 from turtle import Turtle
 from turtle import Screen
 
+# tim is our cursor
 tim = Turtle()
+
+# screen is our window/drawing pad
 screen = Screen()
 
+# self explanatory directional functions below
 def move_forwards():
     tim.forward(10)
 
@@ -16,9 +20,11 @@ def rot_left():
 def rot_right():
     tim.right(5)
 
+# gives us a fresh screen to draw on
 def clear_drawing():
     screen.reset()
 
+# prompts the screen to "listen" for our keystrokes, allowing us to control our cursor by pressing the keys listed below.
 screen.listen()
 screen.onkey(key="w", fun=move_forwards)
 screen.onkey(key="s", fun=move_back)
@@ -26,4 +32,5 @@ screen.onkey(key="a", fun=rot_left)
 screen.onkey(key="d", fun=rot_right)
 screen.onkey(key="c", fun=clear_drawing)
 
+# keeps our window open until we exit, so we can successfully make drawings
 screen.exitonclick()
