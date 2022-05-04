fb7eb5471a8ab6d9df328a9ad77e170d646bca44
finnhewes <finnhewes@gmail.com>
2022-05-04 15:46:37 +0300

    Update server.py

--- Day56_Flask HTML-CSS Rendering--Using Templates/server.py
+++ Day56_Flask HTML-CSS Rendering--Using Templates/server.py
@@ -1,9 +1,9 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/') # the home page route/url
 def home_page():
-    return render_template('index.html')
+    return render_template('index.html')    # render the html template we downloaded from HTML5UP when on the home page
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True)     # ran in debug mode to see my changes quicker/easier.
